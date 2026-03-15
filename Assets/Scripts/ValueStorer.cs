cfe825c59b919073ee6d76af8488b8fa8f9788a2
Ryder <doanryder2005@gmail.com>
2026-03-15 09:54:05 +0700

    Added player, input system and player movement

---
+++ Assets/Scripts/ValueStorer.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class ValueStorer : MonoBehaviour
+{
+    #region PLAYER
+    public static float moveSpeed = 10f;
+    public static float jumpHeight = 27f;
+    public static float lightPush = 9.5f;
+    public static float gravityGround = 10f;
+    public static float gravityMove = 50f;
+
+    public static int maxJumpCount = 2;
+
+    public static float sizeRadiusStill = 0f;
+    public static float sizeRadiusMoving = 0.2f;
+
+    public static Vector2 colliderSizeStill = new Vector2(1f, 1f);
+    public static Vector2 colliderSizeMoving = new Vector2(0.5f, 0.5f);
+
+    public static float playerRespawnTime = 0.75f;
+    #endregion
+
+    #region LAYER MASKS
+    public const string groundLM = "Default";
+    public const string transparentLM = "TransparentFX";
+    #endregion
+
+    #region VALUES
+    public static float coyoteTime = 0.06f;
+    public static float bufferTime = 0.12f;
+    #endregion
+}
